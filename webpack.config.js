128e44c24d42fee1b5ae8f34783a2a9ae61dc4b5
Tom Meagher <thom.meagher@gmail.com>
2016-09-04 12:02:13 -0400

    Move from Grunt to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  watch: true,
+  context: path.join(__dirname, "src"),
+  entry: "./js/app.js",
+    output: {
+    path: __dirname + "/src/",
+    filename: "app.min.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true
+    })
+  ]
+};
